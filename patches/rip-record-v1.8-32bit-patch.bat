96cc780ce0e1090314214a2c82ba826de657f03c
t3rmin8tor <mark.barker2@sky.com>
2017-09-07 11:59:36 +0100

    Add files via upload

---
+++ patches/rip-record-v1.8-32bit-patch.bat
@@ -0,0 +1,38 @@
+@ECHO OFF
+@ECHO CAUTION THIS PATCH IS FOR RIP-RECORD VERSION 1.8
+@ECHO ..
+@ECHO THIS PATCH FIXES ENVIRONMENT ERRORS CAUSED BY WINDOWS UPDATES
+@ECHO ..
+@ECHO ******************
+@ECHO **** WARNING *****
+@ECHO ******************
+@ECHO ..
+@ECHO CONTINUING WILL RESET ALL USER PATHS 
+:PROMPT
+SET /P AREYOUSURE=Are you sure (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+setx PATH "Program Files\Rip-Record\Cygwin\bin;Program Files\Rip-Record\bin;Program Files\Rip-Record\Git\cmd;Program Files\Rip-Record\Git\bin;%userprofile%\git\kodi-playercorefactory\bash-scripts-windows\"
+@ECHO setting Cygwin path....
+
+@ECHO setting application path...
+ 
+@ECHO setting Git path....
+
+@ECHO setting kodi-palyercorefactory path....
+@ECHO ..
+@ECHO ..
+GOTO success 
+
+
+
+
+
+:END
+@ECHO Rip-Record v 1.8 patch terminated 
+exit
+
+:success
+@ECHO success all pathches applied
+@ECHO rip-record patch v1.8 exiting
+pause
+exit
